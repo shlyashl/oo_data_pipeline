a474cd6827ffd8bf91b92b78d792b09d624ee91c
chubukovmn <ay-ya@ya.ru>
2023-01-08 03:39:35 +0300

    Init

--- repository.py
+++ repository.py
@@ -1,17 +1,37 @@
+import importlib
+import sys
+from datetime import timedelta
+
 from dagster import job, op, repository
+from dagster import schedule, OpExecutionContext, ScheduleEvaluationContext, RunRequest
+
+sys.path.extend(['projects/packages'])
+
+# binance-historical-market-data
+sys.path.extend(['projects/packages/binance-historical-market-data'])
+get_bn_history = importlib.import_module('binance-historical-market-data.main').main
 
 
 @op
-def hello_world(context):
-    context.log.warning('jojo RULEZZZ')
-    pass
-
+def update_bn_history_data(context: OpExecutionContext):
+    ed = context.get_tag('date')
+    get_bn_history(ed, ed)
 
 @job
-def hello_world_job():
-    hello_world()
+def update_bn_history_data_job():
+    update_bn_history_data()
 
+@schedule(
+    job=update_bn_history_data_job, cron_schedule='5 0 * * *', execution_timezone='Europe/Moscow'
+)
+def update_bn_history_data_job_schedule(context: ScheduleEvaluationContext):
+    scheduled_date = (
+            context.scheduled_execution_time - timedelta(days=1)
+        ).strftime('%Y-%m-%d')
+    return RunRequest(
+        tags={'date': scheduled_date}
+    )
 
 @repository
-def hello_world_repository():
-    return [hello_world_job]
+def update_bn_history_data_job_schedule():
+    return [update_bn_history_data_job_schedule]
